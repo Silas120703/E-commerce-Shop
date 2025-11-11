2430af8c520b28cfe238717e7a217f9f499478bb
Silas120703 <tuvo120703@gmail.com>
2025-11-11 09:16:24 +0700

    Add Coupon and CouponUsage classes to the database (#9)
    Introduces `Coupon` and `CouponUsage` classes in the
    `VTT_SHOP_DATABASE.Entities` namespace. The `Coupon` class
    manages coupon details with properties for code, description,
    discount types, and usage limits. The `CouponUsage` class
    tracks coupon usage with references to associated `Coupon`,
    `User`, and `Order` entities. Both classes inherit from
    `EntityBase`.

---
+++ VTT_SHOP_DATABASE/Entities/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using VTT_SHOP_SHARED.Database.EntityBase;
+
+namespace VTT_SHOP_DATABASE.Entities
+{
+    public class Coupon : EntityBase
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string DiscountType { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal MaxDiscountValue { get; set; }
+        public decimal MinOrderValue { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int UsageLimit { get; set; }
+        public int UsageLimitPerUser  { get; set; }
+        public int UsedCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
