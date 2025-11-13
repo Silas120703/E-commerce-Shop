00283f6fa86d4ce5740b57cc9513506a54be6781
Silas120703 <tuvo120703@gmail.com>
2025-11-13 15:04:55 +0700

    Add coupon management methods and controller
    Updated `CouponService` with async methods for managing coupons, including retrieval, creation, updating, and activation/deactivation. Added `CouponsController` to handle API requests related to coupons, utilizing the new service methods.

---
+++ VTT_SHOP_API/Controllers/CouponsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VTT_SHOP_CORE.Services;
+using VTT_SHOP_SHARED.DTOs;
+
+namespace VTT_SHOP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponsController : ApiControllerBase
+    {
+        private readonly CouponService _couponService;
+
+        public CouponsController(CouponService couponService) 
+        {
+            _couponService = couponService;
+        }
+
+        [HttpGet("get-by-code")]
+        public async Task<IActionResult> GetCouponByCode([FromQuery] string code)
+        {
+            var result = await _couponService.GetCouponByCodeAsync(code);
+            return HandleResult(result);
+        }
+
+        [HttpPost("create-coupon")]
+        public async Task<IActionResult> CreateCoupon(CouponDTO couponDto)
+        {
+            var result = await _couponService.CreateCouponAsync(couponDto);
+            return HandleResult(result);
+        }
+
+        [HttpPut("update-coupon/{id}")]
+        public async Task<IActionResult> UpdateCoupon(long id, CouponDTO couponDto)
+        {
+            var result = await _couponService.UpdateCouponAsync(id, couponDto);
+            return HandleResult(result);
+        }
+
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetCouponById(long id)
+        {
+            var result = await _couponService.GetCouponByIdAsync(id);
+            return HandleResult(result);
+        }
+
+        [HttpPatch("deactivate-coupon/{id}")]
+        public async Task<IActionResult> DeactivateCoupon(long id)
+        {
+            var result = await _couponService.DeactiveCouponAsync(id);
+            return HandleResult(result);
+        }
+
+        [HttpPatch("activate-coupon/{id}")]
+        public async Task<IActionResult> ActivateCoupon(long id)
+        {
+            var result = await _couponService.ActiveCouponAsync(id);
+            return HandleResult(result);
+        }
+
+    }
+}
